dc08a73bc6420e947757a90f98cbaf43aa87daa1
rituparno-gain <rituparno.gain86@gmail.com>
2018-01-03 14:21:48 -0800

    Number Picker changes.

--- app/src/main/java/com/brewery/app/rigain/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/brewery/app/rigain/tipcalculator/MainActivity.kt
@@ -2,11 +2,27 @@ package com.brewery.app.rigain.tipcalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.NumberPicker
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numberPicker.minValue = 1
+        numberPicker.maxValue = 20
+        numberPicker.wrapSelectorWheel = true
+        numberPicker.setOnValueChangedListener(object : NumberPicker.OnValueChangeListener{
+            override fun onValueChange(p0: NumberPicker?, p1: Int, p2: Int) {
+                Log.i(TAG, "Number Selected: old value=$p1 | new value=$p2")
+            }
+
+        })
+
     }
 }
